16b3c99c70a56c48d2f48f83045396f196b90aa2
ZhengYingHao <zyh1010110@163.com>
2015-04-20 20:02:07 +0800

    modified: add userId attribute

--- app/src/main/java/com/example/zyh/autotakephoto/UserInfo.java
+++ app/src/main/java/com/example/zyh/autotakephoto/UserInfo.java
@@ -2,15 +2,23 @@ package com.example.zyh.autotakephoto;
 
 
 public class UserInfo {
+
+    public static final String NAME = "name";
+    public static final String PASSWORD = "password";
+    public static final String USER_ID = "userId";
+
     private static UserInfo userInfo = new UserInfo();
 
     private String name;
+    private String userId;
 
     private UserInfo() {}
 
     public static UserInfo getUserInfo() { return userInfo; }
 
-    public void setName(String name) { this.name = name; }
+    public synchronized void setName(String name) { this.name = name; }
     public String getName() { return name; }
 
+    public synchronized void setUserId(String userId) { this.userId = userId; }
+    public String getUserId() { return userId; }
 }
