755664ae89f6504f72ac9b189d03af7d5bdd8037
ZhengYingHao <zyh1010110@163.com>
2015-05-06 21:22:11 +0800

    网络状态接收器

---
+++ app/src/main/java/com/example/zyh/autotakephoto/broadcast/receiver/NetStateReceiver.java
@@ -0,0 +1,34 @@
+﻿package com.example.zyh.autotakephoto.broadcast.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+import com.example.zyh.autotakephoto.service.AutoCameraService;
+
+public class NetStateReceiver extends BroadcastReceiver {
+    public static final String TAG = "NetStateReceiver";
+    public static NetworkInfo networkInfo = null;
+    public NetStateReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        /**
+         * 能联网，可以开启截图服务，否则停止。
+         */
+        ConnectivityManager manager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        networkInfo = manager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isAvailable()) {
+            Log.i(TAG, "is available.");
+            AutoCameraService.startable();
+        } else {
+            Log.i(TAG, "no available.");
+            AutoCameraService.stop();
+        }
+    }
+
+}
